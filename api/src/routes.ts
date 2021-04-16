279a747008eacbff7f675e7c9350c9e1c05ffcab
Raphael Rodrigues <rars.developer@gmail.com>
2021-04-16 15:27:45 -0300

    Finalizando users e surveys

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,5 +1,6 @@
 // IMPORTANDO
 import { Router } from "express";
+import { SendMailController } from "./controller/SendMailController";
 import { SurveysController } from "./controller/SurveysController";
 import { UserController } from "./controller/UserController";
 
@@ -7,6 +8,7 @@ import { UserController } from "./controller/UserController";
 const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 // Usuários
 router.post("/users", userController.create);
@@ -14,6 +16,8 @@ router.post("/users", userController.create);
 //Pesquisa de satisfação
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 // EXPORTANDO
 export { router };

950c54d0d5a4a62ba358c11d2fa924f562f90169
Raphael Rodrigues <rars.developer@gmail.com>
2021-04-20 02:24:19 -0300

    Configurando nps

--- api/src/controller/SendMailController.ts
+++ api/src/controller/SendMailController.ts
@@ -32,23 +32,35 @@ class SendMailController {
       });
     }
 
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: [{ user_id: user.id, value: null }],
+      relations: ["user", "survey"],
+    });
+
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+      id: "",
+      link: process.env.URL_MAIL,
+    };
+
+    if (surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id;
+      await SendMailService.execute(email, survey.title, variables, npsPath);
+      return response.json(surveyUserAlreadyExists);
+    }
+
     // Salvar as informações na tabele surveyUser
     const surveyUser = surveysUsersRepository.create({
       user_id: user.id,
       survey_id,
     });
 
-    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
     await surveysUsersRepository.save(surveyUser);
-
-    const variables = {
-      name: user.name,
-      title: survey.title,
-      description: survey.description,
-      user_id: user.id,
-      link: process.env.URL_MAIL,
-    };
+    variables.id = surveyUser.id;
 
     // Enviar e-mail para usuário
     await SendMailService.execute(email, survey.title, variables, npsPath);
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,5 +1,7 @@
 // IMPORTANDO
 import { Router } from "express";
+import { AnswerController } from "./controller/AnswerController";
+import { NpsController } from "./controller/NpsController";
 import { SendMailController } from "./controller/SendMailController";
 import { SurveysController } from "./controller/SurveysController";
 import { UserController } from "./controller/UserController";
@@ -9,6 +11,8 @@ const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 // Usuários
 router.post("/users", userController.create);
@@ -17,7 +21,14 @@ router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
+// E-mail
 router.post("/sendMail", sendMailController.execute);
+
+// Resposta do usuário
+router.get("/answers/:value", answerController.execute);
+
+// NPS
+router.get("/nps/:survey_id", npsController.execute);
 
 // EXPORTANDO
 export { router };
